828da5830d946b9b0a1ce2fb3072dc6a497d1530
unknown <shaharbukra@gmail.com>
2020-09-27 13:55:31 +0300

    fixed bugs

--- README.md
+++ README.md
@@ -1,13 +1,22 @@
-# Node JS Zoom Clone
-
-Credit to [Web Dev Simplified](https://www.youtube.com/watch?v=DvlyzDZDEq4)
-
-### Want to join the BEST JavaScript Course on the planet and learn how to build the projects like Zoom Clone with JavaScript, NodeJS, and MongoDB?
-
-Check out our `Profit With JavaScript Course`    :point_down: 
-
-[Click Here](https://cleverprogrammer.unstacksite.com/pwj?utm_source=github&utm_medium=repo&utm_content=5-aug-live-zoom-clone-youssef-naz&utm_campaign=live-everyday&utm_term=warm)
-
-Made with :heart: by [Naz](https://www.instagram.com/nazdumanskyy/) & [Youssef](https://www.instagram.com/youcef.dev/)
+# Stack used 
 
 
+- Node.js
+- Socket.io
+- peer.js
+- ejs
+- uuid
+- Express.js
+- webRTC
+- Socket.io
+
+# Deploy on Heroku 
+
+- heroku login
+- git add .
+- git commit -am "make it better"
+- git push heroku master
+
+# Video Source Link:
+
+- [Youtube](https://www.youtube.com/watch?v=ZVznzY7EjuY)
--- server.js
+++ server.js
@@ -1,42 +1,75 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-app.use(cors())
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const { ExpressPeerServer } = require('peer');
+const express = require("express");
+const app = express();
+// const cors = require('cors')
+// app.use(cors())
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(server, {
-  debug: true
+  debug: true,
 });
-const { v4: uuidV4 } = require('uuid')
+const User = require("./User.js");
+const { v4: uuidV4 } = require("uuid");
 
-app.use('/peerjs', peerServer);
+let USER_LIST = {};
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.use("/peerjs", peerServer);
 
-app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
-})
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
-})
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidV4()}`);
+});
 
-io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId);
-    // messages
-    socket.on('message', (message) => {
-      //send message to the same room
-      io.to(roomId).emit('createMessage', message)
-  }); 
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
 
-    socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
-    })
-  })
-})
+/**
+ * @description Emits message to all users in a specific room
+ * @param room {string} - Room name/id
+ * @param emit {string} - Name of message to emit
+ * @param message {any} - Message to be sent
+ **/
 
-server.listen(process.env.PORT||3030)
+const sendToAllRoom = (room, emit, message) => {
+  for (let i in USER_LIST) {
+    if (USER_LIST[i].room == room) {
+      USER_LIST[i].socket.emit(emit, message);
+    }
+  }
+};
+
+io.on("connection", (socket) => {
+  //DEBUG: JOIN-ROOM NOT BEING SENT
+  //FIXED: JOIN-ROOM SENT CREATED HELPER FUNCTION ABOVE CALLED SENDTOALLROOM
+  socket.on("join-room", (roomId, userId) => {
+    socket.id = userId;
+    socket.room = roomId;
+    socket.join(roomId);
+
+    sendToAllRoom(roomId, "user-connected", userId);
+    console.log(`joined ${roomId}`);
+
+    USER_LIST[socket.id] = new User({
+      name: `User_${USER_LIST.length}`,
+      socket: socket,
+    });
+  });
+
+  // messages
+  socket.on("message", (data) => {
+    console.log(data.message, USER_LIST[socket.id].room);
+    //send message to the same room
+    sendToAllRoom(USER_LIST[socket.id].room, "createMessage", data);
+  });
+
+  socket.on("disconnect", () => {
+    sendToAllRoom(USER_LIST[socket.id].room, "user-disconnected", socket.id);
+    delete USER_LIST[socket.id];
+  });
+});
+
+server.listen(process.env.PORT || 3030);
+console.log("server started");
